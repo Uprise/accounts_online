bd0ace6f0c035fcb070794a062b941227ff82d92
Sebastian Edwards <s.thursday@gmail.com>
2011-04-01 15:23:55 +1300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccountsOnline::Application.config.secret_token = '44c17b74b173f95171fb1be8b348f391d1f7003f46f85dcf1d53a000a1756c0d42460a5d486040d7855fa210c245f8ab6ba2838e083a53424b933997be037fe5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AccountsOnline::Application.config.session_store :cookie_store, :key => '_accounts_online_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AccountsOnline::Application.config.session_store :active_record_store
