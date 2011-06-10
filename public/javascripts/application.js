f014fada5aaa555d8df694998fc25a4dccad5b54
Sebastian Edwards <s.thursday@gmail.com>
2011-06-10 12:48:41 +1200

    printing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -43,33 +43,7 @@ $(document).ready(function(){
   bartercard();
   $('#bartercard-member').change(bartercard);
   
-  var iFrames = $('embed');
-
-  function iResize() {
-
-          for (var i = 0, j = iFrames.length; i < j; i++) {
-            iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';}
-      }
-
-          if ($.browser.safari || $.browser.opera) { 
-
-             iFrames.load(function(){
-                 setTimeout(iResize, 0);
-         });
-
-             for (var i = 0, j = iFrames.length; i < j; i++) {
-                          var iSource = iFrames[i].src;
-                          iFrames[i].src = '';
-                          iFrames[i].src = iSource;
-         }
-
-          } else {
-             iFrames.load(function() {
-                 this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
-             });
-          }
-
-  });
-  
-  iResize();
+  $('#pdf').load(function(){
+    frames['pdf'].print()
+  })
 })
