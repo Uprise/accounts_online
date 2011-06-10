9d22b8cd8edba48a4c032fb7611ead0585f28d34
Sebastian Edwards <s.thursday@gmail.com>
2011-06-10 13:18:32 +1200

    try

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,5 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s,
-  :print_media_type               => true,
-  :dpi                            => 72
+  :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 }
