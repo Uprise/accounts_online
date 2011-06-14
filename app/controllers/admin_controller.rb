48f6efd3017aacd510f45b59adff19e43bc2282f
Sebastian Edwards <s.thursday@gmail.com>
2011-06-15 10:58:57 +1200

    restrict admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
+  before_filter :check_admin
+  
   def home
     @users = User.all
   end
@@ -6,5 +8,10 @@ class AdminController < ApplicationController
   def edit
     @application_form = ApplicationForm.find(params[:id])
   end
+  
+  private
+    def check_admin
+      redirect_to root_path unless current_user.admin?
+    end
   
 end
