e484ecc26e9d76873dcd3cf7e9c483558970574e
Sebastian Edwards <s.thursday@gmail.com>
2011-08-26 11:08:34 +1200

    setting host on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,8 @@ AccountsOnline::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+  
+  config.action_mailer.default_url_options = { :host => 'accounts-online.heroku.com' }
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
